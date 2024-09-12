c2de220cd278b30ca7d38694cea17a565be47a92
eliud corona <eliudcorona8@gmail.com>
2024-09-12 11:30:46 -0600

    03 operadores

---
+++ conceptos/operators.js
@@ -0,0 +1,38 @@
+//Operadores Matematicos
+//+y-
+//*y/
+//%y** modulo y potencia
+
+console.log(10 + 3);
+console.log(10 -3);
+console.log(10 *3);
+console.log(10 / 3);
+console.log(10 % 3);
+console.log(10 ** 3);
+
+
+
+console.log("Hola" + " " + Mundo);
+
+//
+let a = 1;
+a +=3;
+a -=3;
+a /=3;
+a *=3;
+a %=3;
+a **=3;
+console.log(a);
+
+//operadores de comparacion 
+console.log(10 > 3);
+console.log(10 < 3);
+console.log(10 >= 3);
+console.log(10 <= 3);
+console.log(10 == 3);
+console.log(10 === 3);
+console.log(10 != 3);
+console.log(10 !== 3);
+
+//operadores Logicos
+console.log(true yy true)
